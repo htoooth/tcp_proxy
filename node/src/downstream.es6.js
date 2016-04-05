93bcc019159bc2c28ce4f568f7e4e0b18da0a43f
htoooth <ht.anglenx@gmail.com>
2016-04-06 01:51:50 +0800

    fix farivated project and ui event

--- node/src/downstream.es6.js
+++ node/src/downstream.es6.js
@@ -7,11 +7,14 @@ const client = net.connect({ server: server, port: serverPort }, function() {
   console.log("client connect");
   console.log("send data to server");
   client.write('greeting from client socket\njkfdsklaljkdfskla\nfdskajfkdlsajkldfjslk\n');
+  client.write("fd'sa'df'sa'df'as'\n");
 });
+
+client.setTimeout(6000);
+client.setKeepAlive(true, 3000);
 
 client.on("data", function(data) {
   console.log("receive data: " + data.toString());
-  client.end();
 });
 
 client.on("error", function(err) {
--- node/src/proxy.es6.js
+++ node/src/proxy.es6.js
@@ -1,16 +1,65 @@
 import net from 'net'
 import Promise from 'bluebird'
+import ac from 'async'
+import miss from 'mississippi'
+import es from "event-stream"
 
 const port = 9000;
 const upstreamServer = "localhost";
 const upstreamPort = 9001;
 
-const server = net.createServer(function(downstream) {
-  let upstream = net.connect({ server: upstreamServer, port: upstreamPort }, function() {
-    console.log("upstream connect ok");
+function fromString(string) {
+  return miss.from((size, next) => {
+    if (string.length <= 0) return next(null, null)
+    next(null, string + "\r\n");
   });
+};
 
-  downstream.pipe(upstream).pipe(downstream);
+const upstream = net.connect({ server: upstreamServer, port: upstreamPort }, function() {
+  console.log("upstream connect ok");
+});
+
+upstream.setTimeout(10000);
+upstream.setKeepAlive(true, 3000);
+
+upstream.on("error", function(error) {
+  console.log(error);
+});
+
+const queue = ac.queue((task, cb) => {
+  console.log(`command:${task.cmd}`)
+  fromString(task.cmd)
+    .on("end",function() {
+      cb();
+    })
+    .pipe(upstream, { "end": false })
+    .pipe(task.client, { "end": false });
+}, 1);
+
+const server = net.createServer(function(downstream) {
+  downstream
+    .pipe(es.split())
+    .pipe(miss.through.obj(function(chunk, enc, cb) {
+
+      console.log("receive:" + chunk);
+      let data = {
+        cmd: chunk,
+        client: downstream
+      };
+
+      this.push(data);
+
+      cb();
+    }), { "end": false })
+    .pipe(miss.through.obj((chunk, enc, cb) => {
+      console.log("commnd:" + chunk.cmd);
+
+      queue.push(chunk, (err) => {
+        console.log("finish processing");
+      });
+
+      cb();
+    }), { "end": false });
 });
 
 server.listen(port, () => console.log(`proxy server started on port ${port}`));
